c35f5309916c97db1b61f25ce88a1f4b6caf2d9b
Usman Ali <usmanialiq@gmail.com>
2019-04-15 13:03:57 +0500

    minor changes 2

---
+++ client/build/precache-manifest.661c15be2bcaa72660a49543fc927a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe5f7c8b2a1a49171639",
+    "url": "/static/css/main.6652c5b8.chunk.css"
+  },
+  {
+    "revision": "fe5f7c8b2a1a49171639",
+    "url": "/static/js/main.4ad6ebcf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a6c34a05bc6823e42abb",
+    "url": "/static/js/2.8207a99c.chunk.js"
+  },
+  {
+    "revision": "2cb045b7ae35039cfeb4ae4137cb09f6",
+    "url": "/static/media/sunny_progress_bar.2cb045b7.gif"
+  },
+  {
+    "revision": "7bb4abeab4fb31313bf1712f8de5cd8c",
+    "url": "/index.html"
+  }
+];
